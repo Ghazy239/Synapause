0cae6dd962834bda77a340676862cc4679e670a0
Ghazyy351 <ghazyy351@gmail.com>
2026-07-26 15:04:26 +0700

    Synapause WEB

--- extension/background.js
+++ extension/background.js
@@ -3,9 +3,11 @@ chrome.runtime.onInstalled.addListener(() => {
 });
 
 const monitoredSites = [
-    "youtube.com",
+    "youtube.com/shorts",
     "instagram.com",
-    "tiktok.com"
+    "tiktok.com",
+    "x.com",
+    "threads.com"
 ];
 
 let session = {
@@ -46,7 +48,7 @@ function startTimer(){
             timer.seconds
         );
 
-        if(timer.seconds>=15){
+        if(timer.seconds>=900){
             clearInterval(timer.interval);
             timer.running=false;
             quizRequired=true;
